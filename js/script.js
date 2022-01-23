8f1bcd2fb043176e8b8294010b2ce4b6effa8792
Abdulaziz <87083669+Abdulaziz1234567@users.noreply.github.com>
2022-01-23 16:32:44 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.menu_accardion').hide();
+$('.nav_btn').click(function () {
+  $('.menu_accardion').slideToggle(300);
+ 
+});
+
+$('.HIDIN_BLOCK').hide();
+$('.section_block_right-box_recad_reading').click(function () {
+  $('.HIDIN_BLOCK').slideToggle(300);
+ 
+});
+
+
+$('.filter').click(function () {
+   this.style.color = "red";
+  })
+
